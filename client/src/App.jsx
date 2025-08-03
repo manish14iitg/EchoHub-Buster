c4051a5fca173a3335bf99ed305bc11ac01387fd
manish14iitg <111654062+manish14iitg@users.noreply.github.com>
2025-08-03 21:10:13 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     setAnalysisResult(null);
 
     try {
-      const response = await fetch("http://localhost:5000/api/analyze-news", {
+      const response = await fetch("https://echohub-buster.onrender.com//api/analyze-news", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
